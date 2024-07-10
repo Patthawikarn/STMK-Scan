840ad264e2c218a3149843047c2d2269aa06b4bf
Patthawikarn Panchuen <143491751+Patthawikarn@users.noreply.github.com>
2024-07-10 10:05:32 +0700

    New Scan

---
+++ index.js
@@ -0,0 +1,39 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  var employeeCode = document.getElementById('floatingInput').value;
+  var password = document.getElementById('floatingPassword').value;
+  
+  if(employeeCode.trim() === '' || password.trim() === '') {
+    alert('Please enter both employee code and password.');
+  } else {
+    fetch("http://starmark.work/ProductOnsiteAPI/api/login", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        Username: employeeCode,
+        Password: password
+      })
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.Status === "OK") {
+        alert('Login successful!');
+        window.location.href = "./Scan.html";
+      } else {
+        throw new Error('Login failed. Please try again.');
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error.message);
+      alert('Login failed. Please try again.');
+    });
+  }
+});
